82fcb8ee770f99a2235be65fb12c221ddebf9fad
korealu <643949593@qq.com>
2021-05-06 11:20:42 +0800

    2021-05-06 11:20:42

--- push.sh
+++ push.sh
@@ -6,7 +6,8 @@ baseball=$(
 cd "$baseball" || exit
 #npm run build
 remark=$(date +"%Y-%m-%d %H:%M:%S")
-read -p 'Please input the modified content of this version': note
+read -e "\033[20;1H\033[1;4;32m david use echo say \033[0m Hello World \n": note
+
 git add .
 git commit -m "${note} ${remark}"
 git push
