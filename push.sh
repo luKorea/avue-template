616f2ac8db1efa1dae62a718982eed9e90d6f2fa
korealu <643949593@qq.com>
2021-05-06 11:13:35 +0800

    korea edit push.sh 2021-05-06 11:13:18

--- push.sh
+++ push.sh
@@ -5,9 +5,9 @@ baseball=$(
 )
 cd "$baseball" || exit
 #npm run build
-git add .
 remark=$(date +"%Y-%m-%d %H:%M:%S")
 echo '请输入本次版本修改的内容'
 read note
+git add .
 git commit -m "${note} ${remark}"
 git push
