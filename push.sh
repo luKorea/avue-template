b36b8e5c534ce77f4d68531d1bac18a79c8bf69a
korealu <643949593@qq.com>
2021-05-06 11:12:48 +0800

    korea edit push.sh 2021-05-06 11:12:37

--- push.sh
+++ push.sh
@@ -7,5 +7,7 @@ cd "$baseball" || exit
 #npm run build
 git add .
 remark=$(date +"%Y-%m-%d %H:%M:%S")
-git commit -m "korea avue template ${remark}"
+echo '请输入本次版本修改的内容'
+read note
+git commit -m "${note} ${remark}"
 git push
